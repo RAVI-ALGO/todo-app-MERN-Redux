424affa789b4af3e1ac84fe4247d78dd8ab8986f
RAVI-ALGO <kushwahark724@gmail.com>
2023-08-16 15:06:27 +0530

    second push

--- src/Redux/Actions/index.js
+++ src/Redux/Actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import {AddNewTodo,GetAllTodo,ToggleTodo,DeleteTodo ,ToggleTab} from './actionTypes'
 export const addNewTodo = (data) => async(dispatch)=> {
-        await axios.post('http://localhost:8000/add-todo',{"todo":data}).then((res)=>{
+        await axios.post('https://todo-app-backend-mcbt.onrender.com/add-todo',{"todo":data}).then((res)=>{
             console.log(res)
             dispatch({type:AddNewTodo,payload:res.data})
         })
@@ -11,7 +11,7 @@ export const addNewTodo = (data) => async(dispatch)=> {
 }
 
 export const getTodo =  async(dispatch)=> {
-    await axios.get('http://localhost:8000/get-todo').then((res)=>{
+    await axios.get('https://todo-app-backend-mcbt.onrender.com/get-todo').then((res)=>{
         console.log(res)
         dispatch({type:GetAllTodo, payload:res.data.data})
     })
@@ -21,7 +21,7 @@ export const getTodo =  async(dispatch)=> {
 }
 
 export const toggleTodo = (id) => async(dispatch)=> {
-    await axios.get(`http://localhost:8000/toggle-todo/${id}`).then((res)=>{
+    await axios.get(`https://todo-app-backend-mcbt.onrender.com/toggle-todo/${id}`).then((res)=>{
         console.log(res)
         dispatch({type:ToggleTodo, payload:res.data.data})
     })
@@ -31,7 +31,7 @@ export const toggleTodo = (id) => async(dispatch)=> {
 }
 
 export const deleteTodo = (id) => async(dispatch)=> {
-    await axios.delete(`http://localhost:8000/delete-todo/${id}`).then((res)=>{
+    await axios.delete(`https://todo-app-backend-mcbt.onrender.com/delete-todo/${id}`).then((res)=>{
         console.log(res)
         dispatch({type:DeleteTodo, payload:res.data.data})
     })
